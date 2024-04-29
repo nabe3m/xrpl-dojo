80f70b47a71c6451b4a67d536c3ef9cbe377b261
Nabe3m <dev@mitsutoshiwatanabe.com>
2024-04-29 13:43:07 +0900

    Fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://xrpldojo.dev',
   integrations: [
     starlight({
       title: 'XRPL道場',
